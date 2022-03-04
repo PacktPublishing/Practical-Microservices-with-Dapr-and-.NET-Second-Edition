051b2451ba20035fa38b84b826e0f5a4fb05746d
Francesco Lana <flana@microsoft.com>
2022-03-04 12:49:33 +0100

    chapter5

--- chapter04/sample.microservice.reservation/Item.cs
+++
@@ -1,2 +0,0 @@
-namespace sample.microservice.reservation;
-
--- chapter05/sample.microservice.reservation/Controllers/ReservationController.cs
+++ chapter05/sample.microservice.reservation/Controllers/ReservationController.cs
@@ -1,71 +1,60 @@
-using System;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using Dapr;
-using Dapr.Client;
-using Microsoft.AspNetCore.Mvc;
-using sample.microservice.dto.reservation;
-using sample.microservice.state.reservation;
+namespace sample.microservice.reservation.Controllers;
 
-namespace sample.microservice.reservation.Controllers
+[ApiController]
+public class ReservationController : ControllerBase
 {
-    [ApiController]
-    public class ReservationController : ControllerBase
+    public const string StoreName = "reservationstore";
+
+    /// <summary>
+    /// Method for reserving an item quantity.
+    /// </summary>
+    /// <param name="reservation">Reservation info.</param>
+    /// <param name="daprClient">State client to interact with Dapr runtime.</param>
+    /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+    [HttpPost("reserve")]
+    public async Task<ActionResult<Item>> Reserve(Item reservation, [FromServices] DaprClient daprClient)
     {
-        public const string StoreName = "reservationstore";
+        Console.WriteLine("Enter item reservation");
+        
+        var state = await daprClient.GetStateEntryAsync<ItemState>(StoreName, reservation.SKU);
+        state.Value ??= new ItemState() { SKU = reservation.SKU, Changes = new List<ItemReservation>() };
 
-        /// <summary>
-        /// Method for reserving an item quantity.
-        /// </summary>
-        /// <param name="reservation">Reservation info.</param>
-        /// <param name="daprClient">State client to interact with Dapr runtime.</param>
-        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
-        [HttpPost("reserve")]
-        public async Task<ActionResult<Item>> Reserve(Item reservation, [FromServices] DaprClient daprClient)
+        // update balance
+        state.Value.BalanceQuantity -= reservation.Quantity;
+
+        // record change
+        ItemReservation change = new ItemReservation() { SKU = reservation.SKU, Quantity = reservation.Quantity, ReservedOn = DateTime.UtcNow };
+        state.Value.Changes.Add(change);
+        if (state.Value.Changes.Count > 10) state.Value.Changes.RemoveAt(0);
+
+        await state.SaveAsync();
+
+        // return current balance
+        var result = new Item() {SKU = state.Value.SKU, Quantity= state.Value.BalanceQuantity};
+
+        Console.WriteLine($"Reservation of {result.SKU} is now {result.Quantity}");
+
+        return result;
+    }
+
+    /// <summary>
+    /// Method for reserving an item quantity.
+    /// </summary>
+    /// <param name="reservation">Reservation info.</param>
+    /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+    [HttpGet("balance/{state}")]
+    public ActionResult<Item> Get([FromState(StoreName)]StateEntry<ItemState> state)
+    {
+        Console.WriteLine("Enter item retrieval");
+        
+        if (state.Value == null)
         {
-            Console.WriteLine("Enter item reservation");
-            
-            var state = await daprClient.GetStateEntryAsync<ItemState>(StoreName, reservation.SKU);
-            state.Value ??= new ItemState() { SKU = reservation.SKU, Changes = new List<ItemReservation>() };
-
-            // update balance
-            state.Value.BalanceQuantity -= reservation.Quantity;
-
-            // record change
-            ItemReservation change = new ItemReservation() { SKU = reservation.SKU, Quantity = reservation.Quantity, ReservedOn = DateTime.UtcNow };
-            state.Value.Changes.Add(change);
-            if (state.Value.Changes.Count > 10) state.Value.Changes.RemoveAt(0);
-
-            await state.SaveAsync();
-
-            // return current balance
-            var result = new Item() {SKU = state.Value.SKU, Quantity= state.Value.BalanceQuantity};
-
-            Console.WriteLine($"Reservation of {result.SKU} is now {result.Quantity}");
-
-            return result;
+            return this.NotFound();
         }
+        var result = new Item() {SKU = state.Value.SKU, Quantity= state.Value.BalanceQuantity};
 
-        /// <summary>
-        /// Method for reserving an item quantity.
-        /// </summary>
-        /// <param name="reservation">Reservation info.</param>
-        /// <param name="daprClient">State client to interact with Dapr runtime.</param>
-        /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
-        [HttpGet("balance/{state}")]
-        public ActionResult<Item> Get([FromState(StoreName)]StateEntry<ItemState> state, [FromServices] DaprClient daprClient)
-        {
-            Console.WriteLine("Enter item retrieval");
-            
-            if (state.Value == null)
-            {
-                return this.NotFound();
-            }
-            var result = new Item() {SKU = state.Value.SKU, Quantity= state.Value.BalanceQuantity};
+        Console.WriteLine($"Retrieved {result.SKU} is {result.Quantity}");
 
-            Console.WriteLine($"Retrieved {result.SKU} is {result.Quantity}");
-
-            return result;
-        }
+        return result;
     }
 }
