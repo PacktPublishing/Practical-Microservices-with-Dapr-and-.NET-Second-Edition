781b0d0ea7d36f76a256257196f2b0dc97a5ead1
Francesco Lana <flana@microsoft.com>
2022-03-25 18:07:13 +0100

    chapter8

---
+++ chapter08/sample.microservice.reservationactor.service/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Dapr.Actors.Runtime;
+global using System.Text.Json;
+global using sample.microservice.reservationactor.interfaces;
+global using sample.microservice.state.reservationactor;
+global using sample.microservice.reservationactor.service;
--- chapter08/sample.microservice.reservationactor.service/State/ItemState.cs
+++ chapter08/sample.microservice.reservationactor.service/State/ItemState.cs
@@ -1,21 +1,19 @@
-using Dapr.Actors;
-using System;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+namespace sample.microservice.state.reservationactor;
 
-namespace sample.microservice.state.reservationactor
+public class ItemState
 {
-    public class ItemState
-    {
-        public string SKU {get; set;}
-        public int BalanceQuantity { get; set; }
-        public List<ItemReservation>? Changes { get; set; }
-    }
+    public string? SKU {get; set;}
 
-    public class ItemReservation
-    {
-        public string SKU { get; set; }
-        public int Quantity { get; set; }
-        public DateTime ReservedOn { get; set; }
-    }
+    public int BalanceQuantity { get; set; }
+
+    public List<ItemReservation>? Changes { get; set; }
+}
+
+public class ItemReservation
+{
+    public string? SKU { get; set; }
+
+    public int Quantity { get; set; }
+
+    public DateTime ReservedOn { get; set; }
 }
