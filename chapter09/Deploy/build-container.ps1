8313509685b3a2bd376708a6b5517cdeabf1c9ae
Davide Bedin <davide.bedin@outlook.com>
2022-05-03 22:33:38 +0200

    parameter change

--- chapter09/Deploy/build-container.ps1
+++ chapter09/Deploy/build-container.ps1
@@ -1,33 +1,33 @@
 # meant to be launched from the solution root folder
-# with this file you can build all the project docker images with version and tag
+# with this file you can build all the project docker images with version and defatag
 param (
-    [string]$prefix = "registry", 
-    [string]$tag = "latest"
+    [string]$registry = "registry", 
+    [string]$default = "latest"
     )
 $builddate = "2022-05-02"
 $buildversion = "2.0"
 
 $container = "sample.microservice.order"
-$latest = "{0}/{1}:{2}" -f $prefix, $container, $tag 
-$versioned = "{0}/{1}:{2}" -f $prefix, $container, $buildversion
+$latest = "{0}/{1}:{2}" -f $registry, $container, $default 
+$versioned = "{0}/{1}:{2}" -f $registry, $container, $buildversion
 docker build . -f .\sample.microservice.order\Dockerfile -t $latest -t $versioned --build-arg BUILD_DATE=$builddate --build-arg BUILD_VERSION=$buildversion
 
 $container = "sample.microservice.reservationactor"
-$latest = "{0}/{1}:{2}" -f $prefix, $container, $tag 
-$versioned = "{0}/{1}:{2}" -f $prefix, $container, $buildversion
+$latest = "{0}/{1}:{2}" -f $registry, $container, $default 
+$versioned = "{0}/{1}:{2}" -f $registry, $container, $buildversion
 docker build . -f .\sample.microservice.reservationactor.service\Dockerfile -t $latest -t $versioned --build-arg BUILD_DATE=$builddate --build-arg BUILD_VERSION=$buildversion
 
 $container = "sample.microservice.reservation"
-$latest = "{0}/{1}:{2}" -f $prefix, $container, $tag 
-$versioned = "{0}/{1}:{2}" -f $prefix, $container, $buildversion
+$latest = "{0}/{1}:{2}" -f $registry, $container, $default 
+$versioned = "{0}/{1}:{2}" -f $registry, $container, $buildversion
 docker build . -f .\sample.microservice.reservation\Dockerfile -t $latest -t $versioned --build-arg BUILD_DATE=$builddate --build-arg BUILD_VERSION=$buildversion
 
 $container = "sample.microservice.customization"
-$latest = "{0}/{1}:{2}" -f $prefix, $container, $tag 
-$versioned = "{0}/{1}:{2}" -f $prefix, $container, $buildversion
+$latest = "{0}/{1}:{2}" -f $registry, $container, $default 
+$versioned = "{0}/{1}:{2}" -f $registry, $container, $buildversion
 docker build . -f .\sample.microservice.customization\Dockerfile -t $latest -t $versioned --build-arg BUILD_DATE=$builddate --build-arg BUILD_VERSION=$buildversion
 
 $container = "sample.microservice.shipping"
-$latest = "{0}/{1}:{2}" -f $prefix, $container, $tag
-$versioned = "{0}/{1}:{2}" -f $prefix, $container, $buildversion
+$latest = "{0}/{1}:{2}" -f $registry, $container, $default
+$versioned = "{0}/{1}:{2}" -f $registry, $container, $buildversion
 docker build . -f .\sample.microservice.shipping\Dockerfile -t $latest -t $versioned --build-arg BUILD_DATE=$builddate --build-arg BUILD_VERSION=$buildversion
