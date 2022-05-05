ea8257d2c33addb23487aaf42034c9c243d419f8
Davide Bedin <davide.bedin@outlook.com>
2022-05-05 22:58:11 +0200

    fixed connectionString

--- chapter09/Deploy/deploy-secrets.ps1
+++ chapter09/Deploy/deploy-secrets.ps1
@@ -10,6 +10,6 @@ $reservationinputsecretstorageaccountkey = ""
 kubectl delete secret cosmosdb-secret --namespace $namespace
 kubectl create secret generic cosmosdb-secret --from-literal=masterKey=$cosmosdbsecretmasterkey --from-literal=url=$cosmosdbsecreturl --namespace $namespace
 kubectl delete secret servicebus-secret --namespace $namespace
-kubectl create secret generic servicebus-secret --from-literal=connectionstring=$servicebussecretconnectionstring --namespace $namespace
+kubectl create secret generic servicebus-secret --from-literal=connectionString=$servicebussecretconnectionstring --namespace $namespace
 kubectl delete secret reservationinput-secret --namespace $namespace
 kubectl create secret generic reservationinput-secret --from-literal=connectionString=$reservationinputsecretconnectionString --from-literal=storageAccountKey=$reservationinputsecretstorageaccountkey --namespace $namespace
