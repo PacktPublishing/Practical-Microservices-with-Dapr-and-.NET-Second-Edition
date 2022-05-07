b28890018b628d9e0719b23310f42005f68b377c
Davide Bedin <davide.bedin@outlook.com>
2022-05-07 15:01:17 +0200

    attempt to solve https://github.com/dotnet/dotnet-docker/issues/3274

--- chapter09/Deploy/build-push-container.ps1
+++ chapter09/Deploy/build-push-container.ps1
@@ -5,7 +5,7 @@ param (
     [string]$registry = "registry", 
     [string]$default = "latest"
     )
-$builddate = "2022-05-02"
+$builddate = "2022-05-07"
 $buildversion = "2.0"
 
 $container = "sample.microservice.order"
