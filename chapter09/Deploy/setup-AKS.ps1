f83351e26df085c4c3acfad44bdce76f6d6446d7
Davide Bedin <davide.bedin@outlook.com>
2022-05-09 14:38:47 +0200

    fixed var name

--- chapter09/Deploy/setup-AKS.ps1
+++ chapter09/Deploy/setup-AKS.ps1
@@ -9,24 +9,24 @@ az account set --subscription $subscription
 az group create --name $$rg  --location $location
 
 # create AKS --- no cluster autoscaler https://docs.microsoft.com/en-us/azure/aks/start-stop-cluster  
-az aks create --resource-groue $rg --name $askname  `
+az aks create --resource-groue $rg --name $aksname  `
     --node-count 3 --node-vm-size Standard_D2s_v3 `
     --enable-addons monitoring `
     --vm-set-type VirtualMachineScaleSets `
     --generate-ssh-keys
 
 # stop AKS --- https://docs.microsoft.com/en-us/azure/aks/start-stop-cluster
-az aks stop --name $askname --resource-groue $rg
-az aks show --name $askname  --resource-groue $rg
+az aks stop --name $aksname --resource-groue $rg
+az aks show --name $aksname  --resource-groue $rg
 
 # start AKS --- https://docs.microsoft.com/en-us/azure/aks/start-stop-cluster 
-az aks start --name $askname --resource-groue $rg
-az aks show --name $askname --resource-groue $rg
+az aks start --name $aksname --resource-groue $rg
+az aks show --name $aksname --resource-groue $rg
 
 # kubectl CLI
 az aks install-cli
 
 # access AKS
-az aks get-credentials --name $askname --resource-groue $rg
+az aks get-credentials --name $aksname --resource-groue $rg
 # query AKS
 kubectl get nodes
