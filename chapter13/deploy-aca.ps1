d8fac906162a333941366bb06bada97d1ed2b705
Davide Bedin <davide.bedin@outlook.com>
2022-06-06 22:57:28 +0200

    temporary

--- chapter13/deploy-aca.ps1
+++ chapter13/deploy-aca.ps1
@@ -1,8 +1,6 @@
 $CONTAINERAPPS_ENVIRONMENT = ""
 $RESOURCE_GROUP = ""
-$REGISTRY_LOGIN_SERVER = ""
-$REGISTRY_USERNAME =""
-$REGISTRY_PASSWORD = ""
+$REGISTRY_NAME = ""
 $APPLICATIONINSIGHTS_KEY = ""
 $LOG_ANALYTICS_WORKSPACE = ""
 $LOCATION = "northeurope"
@@ -13,19 +11,27 @@ $LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET=(az monitor log-analytics workspace get-s
 az containerapp env create `
   --name $CONTAINERAPPS_ENVIRONMENT `
   --resource-group $RESOURCE_GROUP `
-  --instrumentation-key $APPLICATIONINSIGHTS_KEY `
+  --dapr-instrumentation-key $APPLICATIONINSIGHTS_KEY `
   --logs-workspace-id $LOG_ANALYTICS_WORKSPACE_CLIENT_ID `
   --logs-workspace-key $LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET `
-  --location $LOCATION
+  --location $LOCATION 
+  
+  # `
+  # --secrets pubsub-servicebus-connectionstring="${PUBSUB_CONNECTIONSTRING}" `
+  #   reservationactorstore-cosmosdb-url="${DB_URL}" `
+  #   reservationactorstore-cosmosdb-masterkey="${DB_MASTERKEY}" `
+  #   reservationstore-cosmosdb-url="${DB_URL}" `
+  #   reservationstore-cosmosdb-masterkey="${DB_MASTERKEY}" `
+  #   customizationstore-cosmosdb-url="${DB_URL}" `
+  #   customizationstore-cosmosdb-masterkey="${DB_MASTERKEY}" `
+  #   orderstore-cosmosdb-url="${DB_URL}" `
+  #   orderstore-cosmosdb-masterkey="${DB_MASTERKEY}"
 
 az containerapp create `
   --name t1-reservationactor `
   --resource-group $RESOURCE_GROUP `
   --environment $CONTAINERAPPS_ENVIRONMENT `
-  --image dbacawdapracr.azurecr.io/sample.microservice.reservationactor:latest `
-  --registry-login-server $REGISTRY_LOGIN_SERVER `
-  --registry-username $REGISTRY_USERNAME `
-  --registry-password $REGISTRY_PASSWORD `
+  --image $REGISTRY_NAME + "/sample.microservice.reservationactor:2.0" `
   --target-port 80 `
   --ingress 'internal' `
   --min-replicas 1 `
@@ -33,17 +39,13 @@ az containerapp create `
   --enable-dapr `
   --dapr-app-port 80 `
   --dapr-app-id reservationactor-service `
-  --dapr-components ./components/reservationactor-components.yaml `
-  --secrets "reservationactorstore-cosmosdb-url=${DB_URL},reservationactorstore-cosmosdb-masterkey=${DB_MASTERKEY}"
+  --dapr-components ./components/reservationactor-components.yaml 
 
 az containerapp create `
   --name t1-reservation `
   --resource-group $RESOURCE_GROUP `
   --environment $CONTAINERAPPS_ENVIRONMENT `
-  --image dbacawdapracr.azurecr.io/sample.microservice.reservation:latest `
-  --registry-login-server $REGISTRY_LOGIN_SERVER `
-  --registry-username $REGISTRY_USERNAME `
-  --registry-password $REGISTRY_PASSWORD `
+  --image $REGISTRY_NAME + "/sample.microservice.reservation:2.0" `
   --target-port 80 `
   --ingress 'internal' `
   --min-replicas 1 `
@@ -51,17 +53,13 @@ az containerapp create `
   --enable-dapr `
   --dapr-app-port 80 `
   --dapr-app-id reservation-service `
-  --dapr-components ./components/reservation-components.yaml `
-  --secrets "reservationpubsub-servicebus-connectionstring=${PUBSUB_CONNECTIONSTRING},reservationstore-cosmosdb-url=${DB_URL},reservationstore-cosmosdb-masterkey=${DB_MASTERKEY}"
+  --dapr-components ./components/reservation-components.yaml 
 
 az containerapp create `
   --name t1-customization `
   --resource-group $RESOURCE_GROUP `
   --environment $CONTAINERAPPS_ENVIRONMENT `
-  --image dbacawdapracr.azurecr.io/sample.microservice.customization:latest `
-  --registry-login-server $REGISTRY_LOGIN_SERVER `
-  --registry-username $REGISTRY_USERNAME `
-  --registry-password $REGISTRY_PASSWORD `
+  --image $REGISTRY_NAME + "/sample.microservice.customization:2.0" `
   --target-port 80 `
   --ingress 'internal' `
   --min-replicas 1 `
@@ -69,17 +67,13 @@ az containerapp create `
   --enable-dapr `
   --dapr-app-port 80 `
   --dapr-app-id customization-service `
-  --dapr-components ./components/customization-components.yaml `
-  --secrets "customizationpubsub-servicebus-connectionstring=${PUBSUB_CONNECTIONSTRING},customizationstore-cosmosdb-url=${DB_URL},customizationstore-cosmosdb-masterkey=${DB_MASTERKEY}"
+  --dapr-components ./components/customization-components.yaml 
 
 az containerapp create `
 --name t1-order `
 --resource-group $RESOURCE_GROUP `
 --environment $CONTAINERAPPS_ENVIRONMENT `
---image dbacawdapracr.azurecr.io/sample.microservice.order:latest `
---registry-login-server $REGISTRY_LOGIN_SERVER `
---registry-username $REGISTRY_USERNAME `
---registry-password $REGISTRY_PASSWORD `
+--image $REGISTRY_NAME + "dbacawdapracr.azurecr.io/sample.microservice.order:2.0" `
 --target-port 80 `
 --ingress 'external' `
 --min-replicas 1 `
@@ -87,8 +81,7 @@ az containerapp create `
 --enable-dapr `
 --dapr-app-port 80 `
 --dapr-app-id order-service `
---dapr-components ./components/order-components.yaml `
---secrets "orderpubsub-servicebus-connectionstring=${PUBSUB_CONNECTIONSTRING},orderstore-cosmosdb-url=${DB_URL},orderstore-cosmosdb-masterkey=${DB_MASTERKEY}"
+--dapr-components ./components/order-components.yaml
 
 $ORDER_BASE_URL = (az containerapp show --resource-group $RESOURCE_GROUP --name t1-order --query "latestRevisionFqdn" -o tsv)
 
