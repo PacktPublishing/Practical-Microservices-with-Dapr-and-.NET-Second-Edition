7834f91965677d978ed990d73fa21928bda9623f
Davide Bedin <davide.bedin@outlook.com>
2022-07-02 21:29:55 +0200

    keda & secrets

--- chapter13/scale-aca.ps1
+++ chapter13/scale-aca.ps1
@@ -12,6 +12,25 @@ az containerapp revision list `
 --name t1-proxy `
 --resource-group $RESOURCE_GROUP
 
+#this should be the connection string to the commonpusub Azure Service Bus topic used by all apps
+$SB_CONNECTION_STRING = ""
+
+#create secrets in each container app
+az containerapp secret set `
+--name t1-reservation `
+--resource-group $RESOURCE_GROUP `
+--secrets pubsub-secret=$SB_CONNECTION_STRING
+
+az containerapp secret set `
+--name t1-customization `
+--resource-group $RESOURCE_GROUP `
+--secrets pubsub-secret=$SB_CONNECTION_STRING
+
+az containerapp secret set `
+--name t1-customization `
+--resource-group $RESOURCE_GROUP `
+--secrets pubsubconnectionstring=$SB_CONNECTION_STRING
+
 # to create a new revision, deriving from the lastest, with some additional changes
 az containerapp revision copy `
 --name t1-proxy `
@@ -22,3 +41,19 @@ az containerapp revision copy `
 --name t1-reservation `
 --resource-group $RESOURCE_GROUP `
 --yaml .\scale\scale-reservationservice.yaml
+
+az containerapp revision copy `
+--name t1-reservationactor `
+--resource-group $RESOURCE_GROUP `
+--yaml .\scale\scale-reservationactorservice.yaml
+
+az containerapp revision copy `
+--name t1-order `
+--resource-group $RESOURCE_GROUP `
+--yaml .\scale\scale-orderservice.yaml
+
+az containerapp revision copy `
+--name t1-customization `
+--resource-group $RESOURCE_GROUP `
+--yaml .\scale\scale-customizationervice.yaml
+
