567825270c879774a353e9f1c8208eeb057e5e98
Davide Bedin <davide.bedin@outlook.com>
2022-08-28 21:02:44 +0200

    resiliency in ch04

--- chapter04/launch.ps1
+++ chapter04/launch.ps1
@@ -1,6 +1,6 @@
 # order-service HTTP + reservation-service HTTP
-dapr run --app-id "order-service" --app-port "5001" --dapr-grpc-port "50010" --dapr-http-port "5010" -- dotnet run --project ./sample.microservice.order/sample.microservice.order.csproj --urls="http://+:5001"
-dapr run --app-id "reservation-service" --app-port "5002" --dapr-grpc-port "50020" --dapr-http-port "5020" -- dotnet run --project ./sample.microservice.reservation/sample.microservice.reservation.csproj --urls="http://+:5002"
+dapr run --app-id "order-service" --app-port "5001" --dapr-grpc-port "50010" --dapr-http-port "5010" --components-path "./components" -- dotnet run --project ./sample.microservice.order/sample.microservice.order.csproj --urls="http://+:5001"
+dapr run --app-id "reservation-service" --app-port "5002" --dapr-grpc-port "50020" --dapr-http-port "5020" --components-path "./components" -- dotnet run --project ./sample.microservice.reservation/sample.microservice.reservation.csproj --urls="http://+:5002"
 
 # order-service HTTP + reservation-service gRPC
 dapr run --app-id "order-service" --app-port "5001" --dapr-grpc-port "50010" --dapr-http-port "5010" -- dotnet run --project ./sample.microservice.order-grpc-client/sample.microservice.order-grpc-client.csproj --urls="http://+:5001"
