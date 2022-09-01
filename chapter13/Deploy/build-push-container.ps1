cfac0c6944b30849df703deda4e463bddb813b61
Davide Bedin <davide.bedin@outlook.com>
2022-09-01 16:23:16 +0200

    updated proxy dockerhub

--- chapter13/Deploy/build-push-container.ps1
+++ chapter13/Deploy/build-push-container.ps1
@@ -6,11 +6,11 @@ param (
     [string]$default = "latest"
     )
 $builddate = "2022-09-01"
-$buildversion = "0.1.6-test"
+$buildversion = "0.1.5"
 
 $container = "sample.proxy"
 $latest = "{0}/{1}:{2}" -f $registry, $container, $default 
 $versioned = "{0}/{1}:{2}" -f $registry, $container, $buildversion
 docker build . -f .\sample.proxy\Dockerfile -t $latest -t $versioned --build-arg BUILD_DATE=$builddate --build-arg BUILD_VERSION=$buildversion
 docker push $versioned
-# docker push $latest 
+docker push $latest 
